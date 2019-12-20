25d99271d08be85a46938f46afd09dbb1b690617
pkjueying <pkjueying@126.com>
2019-12-20 13:29:18 +0800

    Create TLogger.groovy

---
+++ TLogger.groovy
@@ -0,0 +1,18 @@
+class TLogger {
+    private static final String TAG = "zpush-plugin"
+    private static boolean DEBUG = false
+
+    def static enableDebug(boolean enableDebug) {
+        DEBUG = enableDebug
+    }
+
+    def static info(String tag, String info) {
+        if (DEBUG) {
+            println "${TAG}--I--: [${tag}] ${info}"
+        }
+    }
+
+    def static warn(String tag, String info) {
+        println "${TAG}--W--: [${tag}] ${info}"
+    }
+}
